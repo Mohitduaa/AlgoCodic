389a81f8ec9776bb6ba04bf605421d2fd72311b5
Mohitduaa <duamohit10@gmail.com>
2024-09-11 15:28:24 +0530

    first page changes

---
+++ src/Components/Paras.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Hero_section from './Hero_section'
+import Card from './Card'
+import Projects from './Projects'
+import Great_software from './Great_software'
+import Development_approach from './Development_approach'
+
+const Paras = () => {
+  return (
+    <>
+    <Hero_section/>
+    <Card/>
+    <Projects/>
+    <Great_software/>
+    <Development_approach/>
+    </>
+  )
+}
+
+export default Paras
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,19 @@ import Card from "@/Components/Card";
 import Development_approach from "@/Components/Development_approach";
 import Great_software from "@/Components/Great_software";
 import Hero_section from "@/Components/Hero_section";
+import Paras from "@/Components/Paras";
 import Projects from "@/Components/Projects";
+
+
 export default function Home() {
   return (
     <>
-      <Hero_section/>
+      {/* <Hero_section/>
       <Card/>
       <Projects/>
       <Great_software/>
-      <Development_approach/>
+      <Development_approach/> */}
+      <Paras/>
     </>
   );
 }
